68b624f63974d35212e32386d7b62e4c64ab605a
CortoP <vlehuger@student.42.fr>
2014-04-12 15:10:06 +0200

    Player && SpaceMarin classes

---
+++ class/Player.class.php
@@ -0,0 +1,82 @@
+<?PHP
+
+require_once('Ship.class.php');
+
+abstract class Player
+{
+	protected $_name;
+	protected $_id;
+	protected $_ships;  //array of ships [ship_id => ship]
+	static protected $_playerId = 1;
+
+	function __construct($name)
+	{
+		$this->setName($name);
+		$this->setId(self::$_playerId);
+		self::$_playerId += 1;
+	}
+
+	function __toString()
+	{
+		return 'Player: ( Id: ' . $this->_id . ' Name: ' . $this->_name . ' )';
+	}
+
+	function getName()
+	{
+		return ($this->_name);
+	}
+
+	function getId()
+	{
+		return ($this->_id);
+	}
+
+	function getShip($id)
+	{
+		if (isset($this->_ships[$id]))
+		{
+			if ($this->_ship[$id]->getState() != 'dead')
+		   	   return ($this->_ships[$id]);
+		}
+		else
+			return (-1);
+	}
+
+	function setName($name)
+	{
+		if (is_string($name) && $name != '')
+		{
+			$this->_name = $name;
+			return (0);
+		}
+		else
+			return (-1);
+	}
+
+	function setId($id)
+	{
+		if (is_numeric($id) && $id > 0 && $id <= 4)
+		{
+			$this->_id = $id;
+			return (0);
+		}
+		else
+			return (-1);
+	}
+
+	function setShip($ship)
+	{
+		if (function_exists('$ship->am_i_alive'))
+		{
+			if (isset($this->_ships[0]))
+			   array_push($this->_ships, $ship);
+		  	else
+			   $this->_ships = array($ship);
+			return (0);
+		}
+		return (1);
+	}
+}
+
+
+?>
---
+++ class/SpaceMarin.class.php
@@ -0,0 +1,25 @@
+<?PHP
+
+require_once('Player.class.php');
+
+class SpaceMarin extends Player
+{
+	static protected $color = 'blue';
+	static protected $_marins = 1;
+	protected $_marinId;
+
+	function __construct($name)
+	{
+		parent::__construct($name);
+		$this->_marinId = self::$_marins;
+		self::$_marins += 1;
+		echo "I'm ok Marin matricule $this->_id.$this->_marinId!\n";
+	}
+
+	function __toString()
+	{
+		return 'SpaceMarin: ( Id: ' . $this->_id . ' Name: ' . $this->_name . ' )';
+	}
+}
+
+?>
