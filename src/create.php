4c6a64cb4f943eff2c189241da16cb116bbc3dd4
CortoP <vlehuger@student.42.fr>
2014-04-13 19:06:28 +0200

    FISTHammer

--- src/create.php
+++ src/create.php
@@ -1,10 +1,12 @@
 <?PHP
 session_start();
 include('../class/Game.class.php');
-//header('Location: play.php');
 if (!isset($_POST['name']) || !isset($_POST['nbr']) ||!isset($_POST['action'])
 	|| $_POST['name'] == "" || $_POST['nbr'] < 2 || $_POST['nbr'] > 4)
+{
+	echo "<script>window.history.back(-1)</script>";
 	return ;
+}
 $game = new Game(array('name' => $_POST['name'], 'nbrP' => $_POST['nbr']));
 $_SESSION['game'] = 'GetSet';
 $_SESSION['nbr'] = 0;
@@ -12,4 +14,5 @@ $s = serialize($game);
 $fp = fopen("game", "w");
 fwrite($fp, $s);
 fclose($fp);
+echo "<script>window.history.back(-1)</script>";
 ?>
