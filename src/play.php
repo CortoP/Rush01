a792ba2174d2c0dd86d7a58a0bf9e6a1949b04e0
CortoP <vlehuger@student.42.fr>
2014-04-13 11:31:31 +0200

    main css

--- src/play.php
+++ src/play.php
@@ -1,14 +1,28 @@
+<header style="margin-top: 32px">
+<LINK rel="stylesheet" href="../css/index.css" type="text/css" />
 <?PHP
 session_start();
 include('../class/Game.class.php');
 echo '<a href="kill.php">Kill Session</a>';
-if (!isset($_SESSION['game'])){?>
-<form action="create.php" method='POST'>
-	<label for='name'>Name the game</label>
-	<input type='text' name='name' id='name' required/><br/>
-	<label for='nbr'>Number of Players</label>
-	<input type='number' name='nbr' id='nbr' min='2'max='4' required/><br/>
-	<input type='submit' name='action' value='OK'/>
+if (!isset($_SESSION['game'])){
+?>
+<center>
+	<a href="/index.html"> <img src="../Title_Warhammer.png"> </a>
+</center>
+</header>
+<body id='body'>
+<form action="create.php" method='POST' id="container">
+	<div id="create_account">
+		 <label for='name'>Name of the game</label>
+		 <input type='text' name='name' id='name' required/><br/>
+	</div>
+	<div id="Connect">
+		 <label for='nbr'>Number of Players</label>
+		 <input type='number' name='nbr' id='nbr' min='2'max='4' required/><br/>
+	</div>
+	<div id="submit">
+		 <input type='submit' name='action' value='OK'/>
+	</div>
 </form>
 <?PHP
 }
@@ -40,3 +54,4 @@ else
 	fclose($fp);
 }
 ?>
+</body>
