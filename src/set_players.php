5faca1facf0f843dfa017ede968f23610d4bfdb3
lredoban <lredoban@student.42.fr>
2014-04-14 19:19:27 +0200

    gbhl

--- src/set_players.php
+++ src/set_players.php
@@ -1,7 +1,7 @@
 <?PHP
 	include('../class/Game.class.php');
 	session_start();
-//	header('Location: play.php');
+	header('Location: play.php');
 	$game = unserialize($_SESSION['class']);
 //	var_dump($_POST);
 	for ($i = 1; $i <= $game->getNbrP(); $i++)
@@ -16,7 +16,7 @@
 	$game->initPlayers();
 	$maparray['nbP'] = $game->getNbrP();
 	$game->setMap($maparray);
-	var_dump($game);
+//	var_dump($game);
 	$_SESSION['class'] = serialize($game);
 	$_SESSION['game'] = 'GO';
 ?>
